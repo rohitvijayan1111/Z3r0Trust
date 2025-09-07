ccd2f9c3b73c756b84cdcdd1d892b461a16163d2
kavirajmetech <kavirajmetech@gmail.com>
2025-09-07 23:14:36 +0530

    update

--- Agents/app.py
+++ Agents/app.py
@@ -149,6 +149,7 @@ def webhook():
             print("reached inside")
             print(alert)
 
+
             # gemini_response = gemini_model.generate_content(f"{alert}, generate this report as a detailed summary")
             # narrative_summary = gemini_response.text.strip()
             # alert['summary']=narrative_summary
